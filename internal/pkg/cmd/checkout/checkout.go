ddc247f788377c883f6f89b19f788f10e941549c
VedantWankhade <vedantwankhade17@gmail.com>
2024-04-28 23:59:56 +0530

    update checkout for further improvements

--- internal/pkg/cmd/checkout/checkout.go
+++ internal/pkg/cmd/checkout/checkout.go
@@ -9,29 +9,24 @@ import (
 	"github.com/vedantwankhade/fuzzygit/internal/pkg/cmd/git"
 )
 
-func GetBranches() []string {
-	b := git.Cmd("branch")
+func Invoke(flags []string) {
+	b := git.Cmd("branch", flags...)
 	arr := strings.Split(string(b), "\n")
 	var branches []string
 	for _, i := range arr {
 		b := strings.TrimSpace(i)
 		branches = append(branches, strings.Replace(b, "* ", "", 1))
 	}
-	return branches
-}
-
-func Invoke(flags []string) {
-	items := GetBranches()
 	f, err := fzf.New()
 	if err != nil {
 		log.Fatal(err)
 	}
-	idxs, err := f.Find(items, func(i int) string { return items[i] })
+	idxs, err := f.Find(branches, func(i int) string { return branches[i] })
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, i := range idxs {
-		cmd := exec.Command("git", "checkout", items[i])
+		cmd := exec.Command("git", "checkout", branches[i])
 		cmd.Run()
 	}
 }
