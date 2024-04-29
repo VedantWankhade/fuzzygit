2b5d257eefffc8661405329cd47a59fa92bd55b6
VedantWankhade <vedantwankhade17@gmail.com>
2024-04-30 00:01:01 +0530

    checkout commits

--- internal/pkg/cmd/checkout/checkout.go
+++ internal/pkg/cmd/checkout/checkout.go
@@ -1,6 +1,7 @@
 package checkout
 
 import (
+	"fmt"
 	"log"
 	"slices"
 	"strings"
@@ -10,33 +11,75 @@ import (
 )
 
 func Invoke(flags []string) {
-	subcmd := "checkout"
-	b := git.Cmd("branch", flags...)
-	arr := strings.Split(string(b), "\n")
-	var branches []string
-	for _, i := range arr {
-		if strings.Contains(i, "->") {
-			continue
+	if slices.Contains(flags, "-c") {
+		type commit struct {
+			commit string
 		}
-		b := strings.TrimSpace(i)
-		branches = append(branches, strings.Replace(b, "* ", "", 1))
-	}
-	f, err := fzf.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	idxs, err := f.Find(branches, func(i int) string { return branches[i] })
-	if err != nil {
-		log.Fatal(err)
-	}
-	if slices.Contains(flags, "-r") {
-		subcmd = "switch"
+		logMap := make(map[string]commit)
+		var hashes []string
+		// flags = append(flags, "--oneline")
+		res := git.Cmd("log", "--oneline")
+		logLines := strings.Split(res, "\n")
+		for _, l := range logLines {
+			arr := strings.SplitAfterN(l, " ", 2)
+			hash := strings.TrimSpace(arr[0])
+			hashes = append(hashes, hash)
+			logMap[hash] = commit{
+				commit: arr[1],
+			}
+		}
+		f, err := fzf.New()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		idxs, err := f.Find(
+			logLines,
+			func(i int) string { return hashes[i] + " " + logMap[hashes[i]].commit },
+			fzf.WithPreviewWindow(func(i, width, height int) string {
+				filesChanged := git.Cmd("diff", hashes[i], "--name-only")
+				return fmt.Sprintf("Commit message: %s\n\nFiles changed:\n%v", logMap[hashes[i]].commit, filesChanged)
+			}),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		for _, i := range idxs {
-			branches[i] = strings.SplitAfterN(branches[i], "/", 2)[1]
+			fmt.Println(logMap[hashes[i]].commit)
+			fmt.Println(hashes[i])
 		}
-		git.Cmd("fetch")
-	}
-	for _, i := range idxs {
-		git.Cmd(subcmd, branches[i])
+		git.Cmd("checkout", hashes[idxs[0]])
+		git.Cmd("switch", "-c", ("branch-" + hashes[idxs[0]]))
+	} else {
+		subcmd := "checkout"
+		b := git.Cmd("branch", flags...)
+		arr := strings.Split(string(b), "\n")
+		var branches []string
+		for _, i := range arr {
+			if strings.Contains(i, "->") {
+				continue
+			}
+			b := strings.TrimSpace(i)
+			branches = append(branches, strings.Replace(b, "* ", "", 1))
+		}
+		f, err := fzf.New()
+		if err != nil {
+			log.Fatal(err)
+		}
+		idxs, err := f.Find(branches, func(i int) string { return branches[i] })
+		if err != nil {
+			log.Fatal(err)
+		}
+		if slices.Contains(flags, "-r") {
+			subcmd = "switch"
+			for _, i := range idxs {
+				branches[i] = strings.SplitAfterN(branches[i], "/", 2)[1]
+			}
+			git.Cmd("fetch")
+		}
+		for _, i := range idxs {
+			git.Cmd(subcmd, branches[i])
+		}
 	}
 }
